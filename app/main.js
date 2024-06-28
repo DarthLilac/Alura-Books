6c5de711baae815b61fc6d67c26e14a89d1a4ec8
darthlilac <dessalr@live.com>
2024-06-28 16:50:06 -0300

    Desconto

--- app/main.js
+++ app/main.js
@@ -6,6 +6,7 @@ const elementoParaInserirLivros = document.getElementById('livros')
 async function getBuscarLivrosDaAPI() {
     const res = await fetch(endpointDaAPI)
     livros = await res.json()
+    let livrosComDesconto = aplicarDesconto(livros)
     console.table(livros)
-    exibirOsLivrosNaTela(livros)
+    exibirOsLivrosNaTela(livrosComDesconto)
 }
